2b232f28f6ea55aeeb20f99ca8dcf283112baab7
Jana <55837429+jjovcheva@users.noreply.github.com>
2024-03-14 16:03:52 +0100

    Update pk_plots.py

--- pk_plots.py
+++ pk_plots.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import os
 from statistics import mean
 from statsmodels.stats.moment_helpers import cov2corr
+from colossus.cosmology import cosmology
 
 plt.rcParams.update({
     "text.usetex": True,
@@ -69,12 +70,8 @@ def plot_pk(ell, tag):
     
     ax.set_xlabel("$k$ [$h$Mpc$^{-1}$]")
     ax.set_ylabel("$kP_%s(k)$ [$h^{-2}$Mpc$^2$]" %ell)
-    #ax.set_xlim(0.01, 0.21)
-    # ax.set_ylim(-5, 30)
     plt.rcParams['figure.dpi'] = 1000
-    plt.savefig('output/plots/pk%s_%s' %(ell, tag), dpi=1000)
-    
-plot_pk('0', 'LOWZ')
+    plt.savefig('output/plots/pk%s_%s' %(ell, tag), dpi=700)
 
 def plot_corr(cov_matrix, ell):
     '''Plot the correlation matrix.'''
@@ -87,3 +84,28 @@ def plot_corr(cov_matrix, ell):
     plt.imshow(cov2corr(cov_matrix), cmap='viridis')
     plt.colorbar()
     plt.savefig('corr_matrix_%s' %ell, dpi=1000)
+
+def plot_models(krange, cosmo):
+    pk_camb = cosmo.matterPowerSpectrum(krange, z = 0.0, model='camb') 
+    pk_no_bao = cosmo.matterPowerSpectrum(krange, z=0.0, model='eisenstein98_zb')
+    
+    plt.clf()
+    ax = sns.lineplot(x=krange, y=pk_camb * krange, color='r')
+    ax = sns.lineplot(x=krange, y=pk_no_bao * krange, color='blue')
+    ax.set_xlabel("$k$ [$h$Mpc$^{-1}$]")
+    ax.set_ylabel("$kP_0(k)$ [$h^{-2}$Mpc$^2$]")
+    plt.savefig('test_models', dpi=800)
+    
+cosmo = cosmology.setCosmology('planck18') 
+
+def plot_corner():
+    mergedsamples = np.loadtxt('mcmc_samples.txt', skiprows=1)
+    labels = ['b', 'A1', 'A2', 'A3', 'A4', 'A5', 'alpha', 'sigmaNL']
+    fig = corner.corner(mergedsamples, quantiles=[0.16, 0.5, 0.84], plot_density=False,\
+        show_titles=True, title_fmt=".3f", labels=labels)
+    fig.savefig("corner.png", dpi=500)
+
+if __name__ == '__main__':
+    # plot_pk('0', 'CMASSLOWZTOT')
+    # plot_models(krange=np.arange(0.01, 0.5, 0.001), cosmo=cosmo)
+    plot_corner()
